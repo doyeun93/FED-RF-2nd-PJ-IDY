5a2a638eabaee5697e2c28f366d895699d35f1ec
doyeun93 <doyeun93@naver.com>
2024-03-27 17:45:29 +0900

    s

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -23,5 +23,68 @@ function loadFn(){
     moving.style.left = e.pageX + "px";
   }; 
   
-}
+  // 이벤트 대상 구역에 들어올 때 보이기 , 나가면 숨기기 
+  wrap.onmouseenter = () => {
+    moving.style.opacity = 1;
+  }
+
+  wrap.onmouseleave = () => {
+    moving.style.opacity = 0;
+  }
+}  ///////////// loadFn 함수 ////////////
+
+
+/*********************** 프로모션 구역 슬라이드 함수 ***********************/
+function goSlide(evt,sts=true){
+
+  // 1. 오른쪽 버튼인 .ab2인가?
+  let isRbtn = 
+  sts? this.classList.contains("ab2"):true;
+
+  if (isRbtn) {
+    // (1)먼저 왼쪽으로 이동하기
+    proslider.style.left = "-20%";
+    proslider.style.transition = ".4s ease-in-out";
+
+    setTimeout(() => {
+      // (2-1)맨 앞 li 맨 뒤로 이동
+      proslider.appendChild(proslider.querySelectorAll("li")[0]);
+      // 슬라이드 left 값이 -100%이므로 left값을 0으로 변경
+
+      // (2-2)left 값 0으로 변경
+      proslider.style.left = "0";
+      // (2-3)left 트랜지션 없애기
+      proslider.style.transition = "none";
+    }, 600);
+
+  }/////// if문 //////
+  else{
+    let list = proslider.querySelectorAll(li);
+    proslider.insertBefore(list[list.length - 1], list[0]);
+    proslider.style.left = "-20%";
+    proslider.style.transition = "none";
+
+    setTimeout(() => {
+      proslider.style.left = "0";
+      proslider.style.transition = ".4s ease-in-out";
+    }, 0);
+  }
+
+} //////////////////////// goSlide 함수 ////////////////////
+
+let autoI;
+let autoT;
+autoSlide();
+
+// 자동 넘김 호출 함수 //////
+function autoSlide(){
+
+ autoI = setInterval(() => {
+   goSlide(false,false);
+ }, 3000); 
+} //////////////////////// autoSlide 함수 //////////////////
+
+
+
+
 
