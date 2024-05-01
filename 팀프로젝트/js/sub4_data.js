3eebfbc663a165288e6a778c32cfe0b3fb8d15b1
yjhong <qbru@naver.com>
2024-05-01 18:15:44 +0900

    s

--- 팀프로젝트/js/sub4_data.js
+++ 팀프로젝트/js/sub4_data.js
@@ -3,18 +3,88 @@
 const catInfo = [
     {
         idx:1,
+        title:"Woody",
+        desc:`Woody is the same pull-string cowboy sheriff that Andy fell in love
+            with years ago.
+            <br />
+            He’s found a new home with Bonnie and her toys, and he’ll do
+            anything to make sure she’s happy and they’re all taken care of.
+            <br />But an unexpected reunion with his dear friend Bo Peep shows
+            Woody that the world is much bigger than he ever imagined.`,
+        img:["woody","우디"],
+    },
+
+    {
+        idx:2,
+        title:"Buzz Lightyear",
+        desc:`Buzz Lightyear is loyal not only to his owner, but to the friends
+            he’s made along the way—especially his once-rival Woody who’s like a
+            brother to the ace Space Ranger these days.
+            <br />Buzz would do anything to support his pull-string buddy, but
+            when his efforts land him in a carnival game booth as an inadvertent
+            prize, he turns to his inner voice for guidance.`,
+        img:["buzz","버즈"],
+        
+    },
+   
+    {
+        idx:3,
+        title:"Forky",
+        desc:`Forky is not a toy! At least that’s what he thinks. <br />An actual
+            spork-turned-craft-project, Forky is pretty sure that he doesn’t
+            belong in Bonnie’s room. <br />Unfortunately, every time he tries to
+            get away, someone yanks him back into an adventure he’d rather skip.`,
+        img:["forky","포키"],
+    },
+    {
+        idx:4,
+        title:"Bo Peep",
+        desc:`Bo Peep is a long-lost friend of Woody, Buzz and the gang, who
+            always shared a special connection with Woody while residing with
+            her sheep on a lamp base at Andy’s house.
+            <br />After being separated for years, Bo has become chipped and
+            discarded, but her spirit is far from broken. <br />She has grown
+            into an adventure-seeking free spirit whose strength and sarcasm
+            belie her delicate porcelain exterior. <br />When she and Woody are
+            reunited under unlikely circumstances, Bo realizes just how much
+            she’s missed him, and can’t wait to show him what she’s been up to.`,
+        img:["bopeep","보핍"],
+    },
+    {
+        idx:5,
+        title:"Gabby Gabby",
+        desc:`Gabby Gabby is an adorable, talking pull-string doll from the
+            1950s. <br />But unfortunately for her, a manufacturing defect in
+            her pull-string voice box has left her sounding anything but
+            adorable. <br />She has spent more than 60 years forgotten in the
+            depths of a jam-packed antique store—her only companions are a band
+            of voiceless ventriloquist dummies. <br />Gabby Gabby knows someone
+            will want her if only she can find a working voice box to repair
+            hers.`,
+        img:["gabby","개비"],
+    },
+    { 
+        idx:6,
         title:"Ducky and Bunny",
         desc:`Ducky and Bunny are carnival prizes who are eager to be won.
             <br />But when their plans are rudely interrupted, they find
             themselves on an unexpected adventure with a group of toys who have
             no idea what it feels like to be tacked to a prize wall.`,
-        title:["duckybunny","더키버니"],
+        img:["duckybunny","더키버니"],
     },
     {
-        idx:2,
-        title:"",
-        desc:"",
-        title:["",""],
+        idx:7,
+        title:"Duke Caboom",
+        desc:`Duke Caboom is a 1970s toy based on Canada’s greatest stuntman.
+            <br />Riding his powerful Caboom stunt-cycle, Duke is always
+            prepared to show off his stunt poses with confidence and swagger.
+            <br />However, Woody learns quickly that Duke has an Achilles heel:
+            He has never been able to do the awesome stunts advertised in his
+            own toy commercial. <br />For years, Duke has been sitting in an
+            antique store, constantly reliving the failures of his tragic past.`,
+        img:["duke","듀크"],
     },
 
 ];
+
+export {catInfo};
