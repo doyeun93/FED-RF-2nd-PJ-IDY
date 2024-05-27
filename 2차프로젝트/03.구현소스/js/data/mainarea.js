03e2345b9b57c0ee395ac8054cd90dd8798f427f
doyeun93 <doyeun93@naver.com>
2024-05-27 18:16:36 +0900

    s

---
+++ 2차프로젝트/03.구현소스/js/data/mainarea.js
@@ -0,0 +1,44 @@
+[
+    {
+        idx: "레시피",
+        title: "입안 가득, 반달애호박전",
+        imgName: "main1",
+    },
+    {
+        idx: "레시피",
+        title: "돼지등갈비 김치찜",
+        imgName: "main2",
+    },
+    {
+        idx: "솔루션",
+        title: "요리의 기본, 불 조절",
+        imgName: "main3",
+    },
+    {
+        idx: "레시피",
+        title: "고급 중식당의 맛을 집에서! 동파육",
+        imgName: "main4",
+    },
+    {
+        idx: "솔루션",
+        title: "센스있게 딸기 써는 법",
+        imgName: "main5",
+    },
+    {
+        idx: "6",
+        title: "수세미 종류와 활용법",
+        imgName: "main6",
+    },
+    {
+        idx: "7",
+        title: "원팬 햄치즈 토스트",
+        imgName: "main7",
+    },
+    {
+        idx: "8",
+        title: "갈치 손질부터 보관까지",
+        imgName: "main8",
+    },
+    
+
+]
