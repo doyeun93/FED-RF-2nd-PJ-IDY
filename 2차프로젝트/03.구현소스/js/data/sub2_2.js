db0990cba2664f04e04137e649d9d547081fa194
doyeun93 <doyeun93@naver.com>
2024-05-31 18:15:40 +0900

    s

---
+++ 2차프로젝트/03.구현소스/js/data/sub2_2.js
@@ -0,0 +1,39 @@
+[
+    {
+        idx: "1",
+        imgName: "sol1",
+        title: "요리의 기본, 불 조절",
+        type1: "# 요리기본",
+        type2: "# 불조절", 
+    },
+    
+    {
+        idx: "2",
+        imgName: "sol2",
+        title: "센스있게 딸기 써는 법",
+        type1: "# 딸기",
+        type2: "# 과일",
+    
+    },
+
+    {
+        idx: "3",
+        imgName: "sol3",
+        title: " 주방의 기본 도마, 잘 사용하는 팁",
+        type1: "# 조리도구",
+        type2: "# 도마",
+    
+    },
+    
+    {
+        idx: "4",
+        imgName: "sol4",
+        title: "두부 보관법 ",
+        type1: "# 두부",
+        type2: "# 재료 보관",
+    
+    },
+   
+    
+
+]
