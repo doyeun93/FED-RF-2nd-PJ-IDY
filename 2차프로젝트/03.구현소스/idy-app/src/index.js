4fc130b134d40062c011d7583972b7c563fb7ab0
doyeun93 <doyeun93@naver.com>
2024-06-05 18:15:27 +0900

    s

--- 2차프로젝트/03.구현소스/idy-app/src/index.js
+++ 2차프로젝트/03.구현소스/idy-app/src/index.js
@@ -19,7 +19,6 @@ root.render(
             </li>
             <li>
               <a href="#">요리연구소</a>
-            </li>
               <ul className="bt-gnb">
                 <li>
                   <a href="#">레시피</a>
@@ -28,9 +27,9 @@ root.render(
                   <a href="#">솔루션</a>
                 </li>
               </ul>
+            </li>
             <li>
               <a href="#">요리해요</a>
-            </li>
               <ul className="bt-gnb">
                 <li>
                   <a href="#">요리해요</a>
@@ -39,6 +38,7 @@ root.render(
                   <a href="#">고민있어요</a>
                 </li>
               </ul>
+            </li>
             <li>
               <a href="#">WOW이벤트</a>
             </li>
@@ -60,19 +60,70 @@ root.render(
     </top>
 
     {/* <!-- 요리초보가이드 --> */}
-    <header id="header-area"> </header>
+    <header id="header-area"> 
+      <div className=''>
+
+      </div>
+    </header>
 
     {/* <!-- 요리 연구소 --> */}
-    <main id="main-area"> </main>
+    <main id="main-area"> 요리연구소</main>
 
     {/* <!-- 요리해요 --> */}
-    <div id="cook-area"> </div>
+    <div id="cook-area">요리해요 </div>
 
     {/* <!-- 이벤트 구역 --> */}
-    <div id="event-area"> </div>
+    <div id="event-area">이벤트 </div>
 
     {/* <!-- 하단구역 --> */}
-    <footer id="footer-area"> </footer>
+    <footer id="footer-area"> 
+      <div className= "footer-info">
+        <div className="info-area">
+          <ul className='info-top'>
+            <li>
+              <a href="#">이용약관</a>
+            </li>
+            <li>
+              <a href="#">개인정보처리방침</a>
+            </li>
+            <li>
+              <a href="#">이용 가이드</a>
+            </li>
+            <li>
+              <a href="#">ABOUT US</a>
+            </li>
+          </ul>
+          <ul className='info-mid'>
+            <li>샘표식품(주)</li>
+            <li>서울특별시 중구 충무로2(우편번호: 04557)</li>
+          </ul>
+          <span>Copyright © 샘표식품, All Rights Reserved.</span>
+        </div>
+      </div>
+      <div className= "footer-sns">
+        <ul>
+          <li>
+            <a href="#">
+              <img src="" alt="로고어워즈" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="" alt="인스타그램" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="" alt="카카오톡" />
+            </a>
+          </li>
+          <section></section>
+        </ul>
+      </div>
+      <button className='btn-top'>
+        <a href='#'>↑</a>
+      </button>
+    </footer>
 
   </>
 );
