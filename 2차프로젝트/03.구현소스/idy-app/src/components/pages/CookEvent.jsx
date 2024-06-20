03124ce650c4dd92964b4484585ed7faa555911e
doyeun93 <doyeun93@naver.com>
2024-06-20 18:10:11 +0900

    s

--- 2차프로젝트/03.구현소스/idy-app/src/components/pages/CookEvent.jsx
+++ 2차프로젝트/03.구현소스/idy-app/src/components/pages/CookEvent.jsx
@@ -1,13 +1,28 @@
 // WOW이벤트 페이지 컴포넌트 ///
- 
+import React from "react";
 
-export default function CookEvent(){
-    //// 코드 리턴 구역
-    return(
-        <>
-             <h1> WOW이벤트 </h1>
-        </>
-    );
+import "../../css/cookevent.scss";
+
+export default function CookEvent() {
+  //// 코드 리턴 구역
+  return (
+    <>
+      <section className="event-top">
+        <div className="evt-title">
+          <ul>
+            <li>
+              <img src="./image/samie_txt.png" alt="wow" />
+            </li>
+            <li>이벤트</li>
+          </ul>
+        </div>
+        <video src="./image/mv.mp4" style={{width:"100%",height:"70%",objectFit:"cover"}}
+        muted loop autoPlay />
+      </section>
+
+      <section className="event-mid">
+        <h1> WOW이벤트 </h1>
+      </section>
+    </>
+  );
 } ////////////  Event함수 ////////
-
-
--- 2차프로젝트/03.구현소스/idy-app/src/components/pages/CookGuide.jsx
+++ 2차프로젝트/03.구현소스/idy-app/src/components/pages/CookGuide.jsx
@@ -5,6 +5,7 @@ export default function CookGuide(){
     //// 코드 리턴 구역
     return(
         <>
+        {/*<iframe width="1250" height="703" src="https://www.youtube.com/embed/06ZtFT-txeE" title="2021 새미네부엌 TVCF 제품 종합편 &#39;15" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>  */}
             <h1>요리초보가이드</h1>
              {/* 1. 배너 컴포넌트 */}
              {/* <Banner catName="SERIES"/> */}
