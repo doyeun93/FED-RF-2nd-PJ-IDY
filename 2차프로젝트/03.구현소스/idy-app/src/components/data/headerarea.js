c7f99fea6aa22e9069f2172d1472c4eb838217ff
doyeun93 <doyeun93@naver.com>
2024-07-10 16:30:31 +0900

    s

--- 2차프로젝트/03.구현소스/idy-app/src/components/data/headerarea.js
+++ 2차프로젝트/03.구현소스/idy-app/src/components/data/headerarea.js
@@ -15,7 +15,7 @@ export const headerdata = [
     {
         idx: "3",
         imgName: "top3",
-        title: "밑반찬 만드는거? 생각보다 별거 없더라~",
+        title: "밑반찬 만드는거? * 생각보다 별거 없더라~",
     },
     
     {
@@ -27,7 +27,7 @@ export const headerdata = [
     {
         idx: "5",
         imgName: "top5",
-        title: " 냉장고에 남아있는 처치 곤란 식재료, 어떡하죠? ",
+        title: " 냉장고에 남아있는 처치 곤란 식재료, * 어떡하죠? ",
     },
     
     {
@@ -39,19 +39,19 @@ export const headerdata = [
     {
         idx: "7",
         imgName: "top7",
-        title: " 우리집 필수 식재료, 어떻게 보관해야할까?",
+        title: " 우리집 필수 식재료, * 어떻게 보관해야할까?",
     },
     
     {
         idx: "8",
         imgName: "top8",
-        title: "초보도 할 수 있는 그럴듯한 홈파티 음식 ",
+        title: "초보도 할 수 있는 * 그럴듯한 홈파티 음식 ",
     },
     
     {
         idx: "9",
         imgName: "top9",
-        title: "재료 손질이 어려워 요리가 망설여진다면? ",
+        title: "재료 손질이 어려워 * 요리가 망설여진다면? ",
     },
     
 
